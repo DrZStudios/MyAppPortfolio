5430f3be40af2b9b1c7f4f78403b71d5dba10458
DrZStudios <drzstudios@gmail.com>
2015-06-10 07:35:48 -0400

    First draft

--- app/src/main/java/com/apps/drzstudios/myappportfolio/MainActivity.java
+++ app/src/main/java/com/apps/drzstudios/myappportfolio/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,64 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button spot_btn, score_btn, lib_btn, build_btn, reader_btn, capstone_btn;
+
+        spot_btn = (Button) findViewById(R.id.spot_btn);
+        score_btn = (Button) findViewById(R.id.score_btn);
+        lib_btn = (Button) findViewById(R.id.lib_btn);
+        build_btn = (Button) findViewById(R.id.build_btn);
+        reader_btn = (Button) findViewById(R.id.reader_btn);
+        capstone_btn = (Button) findViewById(R.id.capstone_btn);
+
+
+        spot_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnavailable("Spotify Streamer");
+            }
+        });
+
+        score_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnavailable("Scores App");
+            }
+        });
+
+        lib_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnavailable("Library App");
+            }
+        });
+
+        build_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnavailable("Build It Bigger");
+            }
+        });
+
+        reader_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnavailable("XYZ Reader");
+            }
+        });
+
+        capstone_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnavailable("Capstone App");
+            }
+        });
+
+    }
+
+    private void featureUnavailable(String string) {
+        Toast.makeText(getApplicationContext(), (string +" feature is currently under development."),
+                Toast.LENGTH_LONG).show();
     }
 
     @Override
